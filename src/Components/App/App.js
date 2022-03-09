585cbf0c37f80f42fbd0e74a7c992fc4335dd6ba
Piotr Pawlik <59952821+kilwap@users.noreply.github.com>
2022-03-09 18:28:49 +0100

    step 61 completed

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -16,6 +16,7 @@ class App extends React.Component {
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -25,7 +26,7 @@ class App extends React.Component {
     }
 
     tracks.push(track);
-    this.setState({playlistTracks: tracks});
+    this.setState({ playlistTracks: tracks });
   }
 
   removeTrack(track) {
@@ -33,6 +34,10 @@ class App extends React.Component {
     tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
 
     this.setState({ playlistTracks: tracks });
+  }
+
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name });
   }
 
   render() {
@@ -46,7 +51,8 @@ class App extends React.Component {
                           onAdd={this.addTrack}/>
             <Playlist playlistName={this.state.playlistName} 
                       playlistTracks={this.state.playlistTracks}
-                      onRemove={this.removeTrack} />
+                      onRemove={this.removeTrack} 
+                      onNameChange={this.updatePlaylistName} />
           </div>
         </div>
       </div>
